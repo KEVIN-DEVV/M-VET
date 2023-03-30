74aac35d44fccee272763ee191144c8245dbe015
Andrew Kibiwott <kibiandrew@gmail.com>
2023-03-30 14:39:21 +0300

    appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,4 +5,12 @@ class AppointmentsController < ApplicationController
       def new
         @appointment = current_user.appointments.build
       end
+      def create
+        @appointment = current_user.appointments.build(appointment_params)
+        if @appointment.save
+          redirect_to appointments_path, notice: "Appointment was successfully created."
+        else
+          render "new"
+        end
+      end
 end
