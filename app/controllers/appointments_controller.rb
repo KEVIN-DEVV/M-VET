8474ae4e3188315ae73659b4885f0d872107d77f
Andrew Kibiwott <kibiandrew@gmail.com>
2023-03-30 14:38:45 +0300

    appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,4 +2,7 @@ class AppointmentsController < ApplicationController
     def index
         @appointments = current_user.appointments
       end
+      def new
+        @appointment = current_user.appointments.build
+      end
 end
